0ca38b87d50ba7d96fdf1c8cda0f84c168b3ab9d
SamFle <Sam.fleurence.27@eigsi.fr>
2025-06-03 14:40:54 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  root: '.', // très important pour bien pointer vers la racine
+  build: {
+    rollupOptions: {
+      input: './public/index.html' // on dit explicitement à Vite de démarrer ici
+    }
+  }
+})
